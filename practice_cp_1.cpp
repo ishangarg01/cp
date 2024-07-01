dfc6c96b2f15a0afeae50918a056c0b9b6fcd1bd
Ishan <ishangarg0101@gmail.com>
2024-07-01 20:25:13 +0530

    cp file template

--- practice_cp_1.cpp
+++ practice_cp_1.cpp
@@ -15,46 +15,8 @@ void init_code()
 #endif
 }
 
-void dfs(int i, vector<int> &vis, vector<vector<int>> adj, vector<int> arr)
-{
-    vis[i] = 1;
-    arr.push_back(i);
-    for (auto x : adj[i])
-    {
-        if (!vis[i])
-        {
-            dfs(x, vis, adj, arr);
-        }
-    }
-}
-
 void solve()
 {
-    int n, q, a, b;
-    cin >> n >> q;
-    vector<vector<int>> adj(n);
-    for (int i = 0; i < n - 1; i++)
-    {
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-
-    vector<int> vis(n);
-    vector<int> arr;
-    for (int i = 0; i < n; i++)
-    {
-        if (adj[i].size() == 1)
-        {
-            dfs(i, vis, adj, arr);
-            break;
-        }
-    }
-
-    for (auto x : arr)
-    {
-        cout << x;
-    }
 }
 
 int_main main()
@@ -62,7 +24,7 @@ int_main main()
     init_code();
 
     int n = 1;
-    // cin >> n;
+    cin >> n;
 
     while (n--)
     {
